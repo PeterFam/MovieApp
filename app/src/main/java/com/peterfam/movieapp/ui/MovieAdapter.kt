47e47d289c61520e90dd474093f49c9ca3dac8f4
Peter Fam <p.fam@starwallet.com>
2019-03-21 17:07:01 +0200

    second commit - add movie viewModel - add AppDatabase class - add repository layer

--- app/src/main/java/com/peterfam/movieapp/ui/MovieAdapter.kt
+++ app/src/main/java/com/peterfam/movieapp/ui/MovieAdapter.kt
@@ -1,30 +1,37 @@
 package com.peterfam.movieapp.ui
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
 import com.peterfam.movieapp.data.model.Movie
+import com.peterfam.movieapp.R
 
-class MovieAdapter internal constructor(context: Context): RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
-
+class MovieAdapter internal constructor(private val context: Context) :
+    RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     private var movieList = emptyList<Movie>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.MovieViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MovieViewHolder(LayoutInflater.from(context).inflate(R.layout.movie_item_list, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = 5
 
     override fun onBindViewHolder(holder: MovieAdapter.MovieViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+//        Glide.with(context)
+//            .load("https://www.designyourway.net/blog/wp-content/uploads/2018/01/9xuYZ9H.jpg")
+//            .into(holder.movieImg)
+
     }
 
     public class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val movieImg: ImageView = itemView.findViewById(R.id.movie_img)
     }
 
 }
